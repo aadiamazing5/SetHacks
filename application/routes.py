6efe502b5cf42663a0d21d332be329452736ed09
c_java <aurko.nandi@gmail.com>
2020-07-18 22:12:59 -0400

    fix everything :o

--- application/routes.py
+++ application/routes.py
@@ -2,6 +2,12 @@ from application import app
 from flask import render_template, request, json, Response
 
 @app.route("/")
-@app.route("/index")
+@app.route("/index", methods=["POST", "GET"])
 def index():
+    trigger = request.form.get("trigger")
+    if trigger == 1:
+        print(request.form.get("trigger"))
+        print(request.form.get("transportation"))
+        print(request.form.get("type"))
+        print(request.form.get("query"))
     return render_template("index.html", index=True)
