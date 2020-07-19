a625c80b7357431c47ff5491f2cbdd77e5de0be8
c_java <aurko.nandi@gmail.com>
2020-07-19 01:59:57 -0400

    test form

--- application/routes.py
+++ application/routes.py
@@ -5,3 +5,8 @@ from flask import render_template, request, json, Response
 @app.route("/index")
 def index():
     return render_template("index.html", index=True)
+
+@app.route("/showMap")
+def showMap():
+    print("test")
+    return render_template("index.html", index=True)
