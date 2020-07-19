fe100baf1521cab882a134211287363f627f5a9b
c_java <aurko.nandi@gmail.com>
2020-07-19 03:47:56 -0400

    basic input to address

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,33 @@
-from application import app
+from application import app, foursquare
 from flask import render_template, request, json, Response
 
 @app.route("/")
-@app.route("/index")
+@app.route("/index", methods=["POST", "GET"])
 def index():
-    return render_template("index.html", index=True)
+
+    address = request.args.get("query")
+    venue_type = request.args.get("type")
+    transpo_type = request.args.get("transport")
+
+    params_list = foursquare.returnParams(address, venue_type, transpo_type)
+
+    print(address, venue_type, transpo_type)
+
+    address = params_list[0]
+    venue_type = params_list[1]
+    rad = params_list[2]
+
+    json_venues = foursquare.getVenues(foursquare.CLIENT_ID, foursquare.CLIENT_SECRET, address, foursquare.VERSION, venue_type, rad, foursquare.LIMIT)
+    print(json_venues)
+    return render_template("index.html", index=True, json_data=json_venues)
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+@app.route('/shutdown')
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
